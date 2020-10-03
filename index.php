01f5adefb9fed643cb78f7515478cc9dc2f86f3b
lizasetre <60122669+lizasetre@users.noreply.github.com>
2020-10-03 18:18:40 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+<title>CSS Website Layout</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  margin: 0;
+}
+.column {
+  float: left;
+  width: 33.33%;
+}
+.footer {
+  background-color: #F1F1F1;
+  text-align: center;
+  padding: 10px;
+}
