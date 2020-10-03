2f0b952d2b5ff4aaca862d844f8d447f2b1f9616
lizasetre <60122669+lizasetre@users.noreply.github.com>
2020-10-03 21:59:51 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,20 @@ $page = (isset($_GET['page']) && $_GET['page'] != '') ? $_GET['page']: '';
 	    
      <div class="header">
      <h1>Smile Studio</h1>
+     <div class='camera'>
+     <div class='tower1'>
+     </div>
+     <div class='effiel'>
+     </div>
+     <div class='tower3'>
+     </div>
+     <div class='tower4'>
+     </div>
+     <div class='outer-circle'>
+     </div>
+     <div class='aah'>
+     </div>
+     </div>     
      <section>
      <div id="insta_wrapper">
     <div class="viewfinder"></div>
