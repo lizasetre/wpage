0c2675cb35a975208e75de116cc3a8ef86f89c4a
lizasetre <60122669+lizasetre@users.noreply.github.com>
2020-10-03 22:32:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -24,11 +24,27 @@ $page = (isset($_GET['page']) && $_GET['page'] != '') ? $_GET['page']: '';
         <a href="index.php?page=loginsignup">Login/Signup</a>
 	<a href="index.php?page=booknow">Book Now</a>
       </div>
+	      
 
     <div id="content">         
 	    
 <br><br><br><br><br><br><br><br><br><br>
 	<div class="footer">
       </div>
+   
+   <div class="social-icons"> 
+   <a class="social-icon social-icon--twitter">
+    <i class="fa fa-twitter"></i>
+    <div class="tooltip">Twitter</div>
+  </a>
+  <a class="social-icon social-icon--instagram">
+    <i class="fa fa-instagram"></i>
+    <div class="tooltip">Instagram</div>
+  </a>
+  <a class="social-icon social-icon--facebook">
+    <i class="fa fa-facebook"></i>
+    <div class="tooltip">Facebook</div>
+  </a>
+</div>
     </body>
 </html>
